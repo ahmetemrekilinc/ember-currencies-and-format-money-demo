1e6bcc6affde93c4cc1c6e2b1cf7849b48ca80bc
ahmetemrekilinc <aekilinc@gmail.com>
2019-01-11 09:04:08 +0300

    first commit

---
+++ app/components/showcase.js
@@ -0,0 +1,27 @@
+//BEGIN-SNIPPET showcase
+import Component from '@ember/component';
+import {set} from '@ember/object';
+import { currenciesArray } from 'ember-currencies-and-format-money/currencies';
+
+export default Component.extend({
+
+  allCurrencies: currenciesArray,
+
+  init(){
+    this._super(...arguments);
+    this.set('myMoneyValue', {amount: 324, currency: 'USD'});
+  },
+
+  actions: {
+    updateAmount(event){
+      let amount = event.target.value;
+      this.set('myMoneyValue', {amount: amount, currency: this.get('myMoneyValue.currency')});
+    },
+    updateCurrency(event){
+      let currency = event.target.value;
+      this.set('myMoneyValue', {amount: this.get('myMoneyValue.amount'), currency: currency});
+    }
+  }
+
+});
+//END-SNIPPET
