b8ce5166456c12d9decf2dadb5e684f4fcf70997
esrgrlk <esragurluk@gmail.com>
2020-01-16 09:35:45 +0300

    ember-cli is upgraded to 3.15.0

--- app/components/showcase.js
+++ app/components/showcase.js
@@ -1,26 +1,25 @@
 //BEGIN-SNIPPET showcase
-import Component from '@ember/component';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 import { currenciesArray } from 'ember-currencies-and-format-money/currencies';
 
-export default Component.extend({
+export default class Showcase extends Component {
 
-  allCurrencies: currenciesArray,
+  allCurrencies = currenciesArray;
 
-  init(){
-    this._super(...arguments);
-    this.set('myMoneyValue', {amount: 324, currency: 'USD'});
-  },
+  @tracked myMoneyValue = {amount: 324, currency: 'USD'};
 
-  actions: {
-    updateAmount(event){
-      let amount = event.target.value;
-      this.set('myMoneyValue', {amount: amount, currency: this.get('myMoneyValue.currency')});
-    },
-    updateCurrency(event){
-      let currency = event.target.value;
-      this.set('myMoneyValue', {amount: this.get('myMoneyValue.amount'), currency: currency});
-    }
+  @action
+  updateAmount(event){
+    let amount = event.target.value;
+    this.myMoneyValue = {amount: amount, currency: this.myMoneyValue.currency};
   }
 
-});
+  @action
+  updateCurrency(event){
+    let currency = event.target.value;
+    this.myMoneyValue = {amount: this.myMoneyValue.amount, currency: currency};
+  }
+}
 //END-SNIPPET
